4c8638f08324ccc3d0135127f2702538a1024543
brazz7762 <rasmussen.benjaminj@gmail.com>
2021-11-30 20:03:21 -0600

    resolves #11

--- server/index.js
+++ server/index.js
@@ -10,8 +10,22 @@ app.use(express.static('client/dist'));
 
 let port = 1121;
 
+
+
 app.listen(port, function(){
   console.log(`I am listening on port ${port}`)
+})
+
+app.post('/api/writePrice', async function (req, res) {
+  try{
+    console.log('running in get right now')
+    var test = 4600;
+    var symbol = 'ETH';
+    models.writeGenPrice(test, symbol);
+    console.log('Yayyyy this worked!')
+  } catch {
+    console.log('Error doing the writing/getting')
+  }
 })
 
 
